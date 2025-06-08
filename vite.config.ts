f9451d13faf965141f4aa846f9a8dd6c5face760
Your Name <your.email@example.com>
2025-06-08 11:00:18 -0400

    Alias uuid to ESM build and optimizeDeps include both uuid and date-fns

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,11 @@ import { resolve } from 'path'
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    include: ['uuid']
+    include: ['uuid', 'date-fns']
   },
   resolve: {
     alias: {
-      uuid: resolve(__dirname, 'node_modules/uuid/dist/esm-browser/index.js'),
+      uuid: resolve(__dirname, 'node_modules/uuid/dist/esm/index.js'),
       'date-fns': resolve(__dirname, 'node_modules/date-fns/esm/index.js')
     }
   },
